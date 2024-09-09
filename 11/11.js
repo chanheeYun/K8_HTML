ff4491be5625a65ea2ae01c43517fb30a88fb073
chanheeYun <ych1782@naver.com>
2024-09-09 16:47:01 +0900

    JS Object

--- 11/11.js
+++ 11/11.js
@@ -3,6 +3,37 @@ document.addEventListener('DOMContentLoaded', () => {
    const sec1 = document.querySelectorAll('.secBt1');
    const sec2 = document.querySelectorAll('.secBt2');
    const sec3 = document.querySelectorAll('.secBt3');
+   const secBt41 = document.querySelector('#secBt41') ;
+   const secBt42 = document.querySelector('#secBt42') ;
+
+   //object 생성
+   let obj = {
+      apple:'🍎', 
+      carrot:'🥕', 
+      banana:'🍌', 
+   } ;
+
+   let jsonDt ;
+   secBt41.addEventListener('click', (e)=>{
+      e.preventDefault();
+
+      jsonDt = JSON.stringify(obj) ;
+
+      console.log('obj = ', obj) ;
+      console.log('obj = ', obj['apple']) ;
+      console.log('jsonDt = ', jsonDt) ;
+      console.log('jsonDt = ', jsonDt["apple"]) ;//json형태로는 실행되지 않음
+   });
+   
+   secBt42.addEventListener('click', (e)=>{
+      e.preventDefault();
+
+      obj2 = JSON.parse(jsonDt) ;
+
+      console.log('obj2 = ', obj2) ;
+      console.log('obj2 = ', obj['apple']) ;
+   });
+
 
    //추가
    for (let bt of sec1) {
